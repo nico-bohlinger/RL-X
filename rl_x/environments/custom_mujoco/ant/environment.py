17c4d573fee29447b297d92fd4b635e3023ee9e2
Nico <nico.bohlinger@gmail.com>
2023-09-26 23:53:29 +0200

    Continue working on the custom MuJoCo env

--- rl_x/environments/custom_mujoco/ant/environment.py
+++ rl_x/environments/custom_mujoco/ant/environment.py
@@ -1,39 +1,107 @@
 from pathlib import Path
 import mujoco
 import numpy as np
-from viewer import MujocoGlfwViewer
+from scipy.spatial.transform import Rotation
+
+from rl_x.environments.custom_mujoco.ant.viewer import MujocoViewer
 
 
-xml_path = (Path(__file__).resolve().parent / "data" / "ant.xml").as_posix()
+class Ant:
+    def __init__(self, horizon=1000, render=False):
+        self.horizon = horizon
+        self.render = render
 
-model = mujoco.MjModel.from_xml_path(xml_path)
+        xml_path = (Path(__file__).resolve().parent / "data" / "ant.xml").as_posix()
+        self.model = mujoco.MjModel.from_xml_path(xml_path)
+        self.data = mujoco.MjData(self.model)
 
-data = mujoco.MjData(model)
+        self.nr_substeps = 1
+        self.nr_intermediate_steps = 1
+        self.dt = self.model.opt.timestep * self.nr_substeps * self.nr_intermediate_steps
 
-def user_warning_raise_exception(warning):
-    if 'Pre-allocated constraint buffer is full' in warning:
-        raise RuntimeError(warning + 'Increase njmax in mujoco XML')
-    elif 'Pre-allocated contact buffer is full' in warning:
-        raise RuntimeError(warning + 'Increase njconmax in mujoco XML')
-    elif 'Unknown warning type' in warning:
-        raise RuntimeError(warning + 'Check for NaN in simulation.')
-    else:
-        raise RuntimeError('Got MuJoCo Warning: ' + warning)
+        if self.render:
+            self.viewer = MujocoViewer(self.model, self.dt)
 
-mujoco.set_mju_user_warning(user_warning_raise_exception)
+        self.target_local_x_velocity = 1.0
+        self.target_local_y_velocity = 0.0
 
-dt = 0.01
-nr_substeps = 1
-nr_intermediate_steps = 1
 
-viewer = MujocoGlfwViewer(model, dt * nr_substeps * nr_intermediate_steps)
+    def reset(self):
+        self.episode_step = 0
+        
+        qpos = np.zeros(self.model.nq)
+        qpos[2] = 0.75  # z position
+        rotation_angles = np.array([0.0, 0.0, 0.0])  # yaw, pitch, roll
+        rotation_quaternion = Rotation.from_euler("xyz", rotation_angles).as_quat()
+        qpos[3:7] = rotation_quaternion
 
-for episode in range(100):
-    mujoco.mj_resetData(model, data)
-    for step in range(1000):
-        action = np.random.randn(model.nu) * 30.0
-        for _ in range(nr_intermediate_steps):
-            data.ctrl = action
-            mujoco.mj_step(model, data, nr_substeps)
-        viewer.render(data, False)
-    print('Episode', episode)
+        qvel = np.zeros(self.model.nv)
+
+        self.data.qpos[:] = qpos
+        self.data.qvel[:] = qvel
+        mujoco.mj_forward(self.model, self.data)
+
+        if self.render:
+            self.viewer.render(self.data)
+
+        return self.get_observation(), {}
+
+
+    def step(self, action):
+        for _ in range(self.nr_intermediate_steps):
+            self.data.ctrl = action
+            mujoco.mj_step(self.model, self.data, self.nr_substeps)
+
+        if self.render:
+            self.viewer.render(self.data)
+        
+        self.episode_step += 1
+
+        next_state = self.get_observation()
+        reward, r_info = self.get_reward()
+        terminated = False
+        truncated = self.episode_step >= self.horizon
+        info = {**r_info}
+
+        return next_state, reward, terminated, truncated, info
+
+
+    def get_observation(self):
+        joint_positions = self.data.qpos[7:]
+        joint_velocities = self.data.qvel[6:]
+        local_angular_velocities = self.data.qvel[3:6]
+        projected_gravity_vector = np.matmul(self.data.body("torso").xmat.reshape(3, 3).T, np.array([0.0, 0.0, -1.0]))
+        observation = np.concatenate([joint_positions, joint_velocities, local_angular_velocities, projected_gravity_vector])
+        return observation
+
+
+    def get_reward(self):
+        rotation_quaternion = self.data.qpos[3:7]
+        yaw_angle = Rotation.from_quat(rotation_quaternion).as_euler("xyz")[0]
+        target_global_x_velocity = self.target_local_x_velocity * np.cos(yaw_angle) - self.target_local_y_velocity * np.sin(yaw_angle)
+        target_global_y_velocity = self.target_local_x_velocity * np.sin(yaw_angle) + self.target_local_y_velocity * np.cos(yaw_angle)
+        target_global_xy_velocity = np.array([target_global_x_velocity, target_global_y_velocity])
+        current_global_xy_velocity = self.data.qvel[:2]
+        xy_velocity_difference_norm = np.sum(np.square(target_global_xy_velocity - current_global_xy_velocity))
+        tracking_xy_velocity_command_reward = np.exp(-xy_velocity_difference_norm / 0.25)
+
+        reward = tracking_xy_velocity_command_reward
+
+        info = {
+            "reward/track_xy_vel_cmd": tracking_xy_velocity_command_reward,
+            "env_info/xy_vel_diff_norm": xy_velocity_difference_norm,
+        }
+
+        return reward, info
+
+
+if __name__ == "__main__":
+    env = Ant(render=True)
+    for episode in range(100):
+        state, info = env.reset()
+        done = False
+        while not done:
+            action = np.random.randn(env.model.nu)
+            next_state, reward, terminated, truncated, info = env.step(action)
+            done = terminated | truncated
+        print('Episode', episode)
