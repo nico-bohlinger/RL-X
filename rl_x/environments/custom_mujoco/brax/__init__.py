63b4e6dd255273efe3d1ab74bf0177deac1b981e
Nico <nico.bohlinger@gmail.com>
2024-11-26 14:48:41 +0100

    Delete the brax env

--- rl_x/environments/custom_mujoco/brax/__init__.py
+++
@@ -1,8 +0,0 @@
-from rl_x.environments.environment_manager import extract_environment_name_from_file, register_environment
-from rl_x.environments.custom_mujoco.brax.create_env import create_env
-from rl_x.environments.custom_mujoco.brax.default_config import get_config
-from rl_x.environments.custom_mujoco.brax.general_properties import GeneralProperties
-
-
-BRAX = extract_environment_name_from_file(__file__)
-register_environment(BRAX, get_config, create_env, GeneralProperties)
