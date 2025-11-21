04de479f4bd2950f4fb41e668c96905fea642dfd
Nico <nico.bohlinger@gmail.com>
2025-11-21 21:18:32 +0100

    Make 4096 envs default for mujoco playground

--- rl_x/environments/mujoco_playground/go1_joystick_flat_terrain/default_config.py
+++ rl_x/environments/mujoco_playground/go1_joystick_flat_terrain/default_config.py
@@ -8,7 +8,7 @@ def get_config(environment_name):
 
     config.type = "Go1JoystickFlatTerrain"
     config.seed = 1
-    config.nr_envs = 1
+    config.nr_envs = 4096
     config.use_domain_randomization = False
     config.render = False
 
