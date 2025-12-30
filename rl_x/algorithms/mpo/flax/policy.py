a0e112429c4f24d073d6acfabc3b153e6a3b81f0
Nico <nico.bohlinger@gmail.com>
2025-12-30 16:54:48 +0100

    Start updating mpo flax implementation

--- rl_x/algorithms/mpo/flax/policy.py
+++ rl_x/algorithms/mpo/flax/policy.py
@@ -18,24 +18,23 @@ def get_policy(config, env):
     policy_observation_indices = getattr(env, "policy_observation_indices", jnp.arange(env.single_observation_space.shape[0]))
 
     if action_space_type == ActionSpaceType.CONTINUOUS and observation_space_type == ObservationSpaceType.FLAT_VALUES:
-        return (Policy(env.single_action_space.shape, config.algorithm.init_stddev, config.algorithm.min_stddev, config.algorithm.nr_hidden_units, policy_observation_indices),
-                get_processed_action_function(jnp.array(env.single_action_space.low), jnp.array(env.single_action_space.high)))
+        return (Policy(env.single_action_space.shape, config.algorithm.policy_init_scale, config.algorithm.policy_min_scale, config.algorithm.nr_hidden_units, policy_observation_indices),
+                get_processed_action_function(config.algorithm.action_clipping, config.algorithm.action_rescaling, jnp.array(env.single_action_space.low), jnp.array(env.single_action_space.high)))
 
 
 def uniform_scaling(scale, dtype = jnp.float_):
     def init(key, shape, dtype=dtype):
-        input_size = np.product(shape[:-1])
         dtype = dtypes.canonicalize_dtype(dtype)
-        named_shape = core.as_named_shape(shape)
-        max_val = jnp.sqrt(3 / input_size) * scale
-        return random.uniform(key, named_shape, dtype, -1) * max_val
+        fan_in = shape[0]
+        max_val = jnp.sqrt(3 / fan_in) * scale
+        return random.uniform(key, shape, dtype, -max_val, max_val)
     return init
 
 
 class Policy(nn.Module):
     as_shape: Sequence[int]
-    init_stddev: float
-    min_stddev: float
+    policy_init_scale: float
+    policy_min_scale: float
     nr_hidden_units: int
     policy_observation_indices: Sequence[int]
 
@@ -53,13 +52,16 @@ class Policy(nn.Module):
         mean = nn.Dense(np.prod(self.as_shape).item(), kernel_init=variance_scaling(1e-4, "fan_in", "truncated_normal"))(x)
 
         stddev = nn.Dense(np.prod(self.as_shape).item(), kernel_init=variance_scaling(1e-4, "fan_in", "truncated_normal"))(x)
-        stddev = self.min_stddev + (jax.nn.softplus(stddev) * self.init_stddev / jax.nn.softplus(0.0))  # Cholesky factor from MPO paper, implemented like in https://github.com/deepmind/acme/blob/master/acme/jax/networks/distributional.py
+        stddev = self.policy_min_scale + (jax.nn.softplus(stddev) * self.policy_init_scale / jax.nn.softplus(0.0))  # Cholesky factor from MPO paper, implemented like in https://github.com/deepmind/acme/blob/master/acme/jax/networks/distributional.py
 
         return mean, stddev
 
 
-def get_processed_action_function(env_as_low, env_as_high):
+def get_processed_action_function(action_clipping, action_rescaling, env_as_low, env_as_high):
     def get_clipped_and_scaled_action(action, env_as_low=env_as_low, env_as_high=env_as_high):
-        clipped_action = jnp.clip(action, -1, 1)
-        return env_as_low + (0.5 * (clipped_action + 1.0) * (env_as_high - env_as_low))
+        if action_clipping:
+            action = jnp.clip(action, -1, 1)
+        if action_rescaling:
+            action = env_as_low + (0.5 * (action + 1.0) * (env_as_high - env_as_low))
+        return action
     return jax.jit(get_clipped_and_scaled_action)
