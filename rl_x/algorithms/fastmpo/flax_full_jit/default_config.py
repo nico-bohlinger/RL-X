a9830eed36c84f966d1e060794b2ca1106fed1a3
Nico <nico.bohlinger@gmail.com>
2026-02-09 16:48:55 +0100

    Improve default FastMPO config

--- rl_x/algorithms/fastmpo/flax_full_jit/default_config.py
+++ rl_x/algorithms/fastmpo/flax_full_jit/default_config.py
@@ -12,8 +12,8 @@ def get_config(algorithm_name):
     config.critic_network_type = "fastsac"  # fastsac, fasttd3, mpo
     config.dual_critic = True
     config.policy_network_type = "fastsac"  # fastsac, fasttd3, mpo
-    config.action_clipping = True
-    config.action_rescaling = "fastsac"  # "none", "fastsac", "normal"
+    config.action_clipping = False
+    config.action_rescaling = "none"  # "none", "fastsac", "normal"
     config.policy_learning_rate = 3e-4
     config.critic_learning_rate = 3e-4
     config.dual_learning_rate = 1e-2
@@ -27,7 +27,7 @@ def get_config(algorithm_name):
     config.adam_beta2 = 0.95  # default: 0.999
     config.max_grad_norm = 40.0
     config.collect_data_with_online_policy = False
-    config.action_sampling_number = 20
+    config.action_sampling_number = 4
     config.epsilon_non_parametric = 0.1
     config.epsilon_parametric_mu = 0.01
     config.epsilon_parametric_sigma = 1e-6
@@ -40,14 +40,14 @@ def get_config(algorithm_name):
     config.min_log_temperature = -18.0
     config.min_log_alpha = -18.0
     config.policy_init_scale = 0.5
-    config.policy_min_scale = 1e-6
+    config.policy_min_scale = 0.1
     config.batch_size = 8192  # fastsac: 8192, fasttd3: 32768
     config.buffer_size_per_env = 1024  # fastsac: 1024, fasttd3: 10240
     config.learning_starts = 10  # times nr_envs
     config.v_min = -20.0  # fastsac: -20.0, fasttd3: -10.0
     config.v_max = 20.0  # fastsac: 20.0, fasttd3: 10.0
     config.critic_tau = 0.125  # fastsac: 0.125, fasttd3: 0.1
-    config.policy_tau = 0.01
+    config.policy_tau = 0.3
     config.gamma = 0.97
     config.nr_atoms = 101
     config.n_steps = 1
