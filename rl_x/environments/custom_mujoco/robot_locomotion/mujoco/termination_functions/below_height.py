570291f108fc61e72b91f0675fdb725df6709c54
Nico <nico.bohlinger@gmail.com>
2026-07-21 06:14:05 +0200

    Keep locomotion fall termination active

--- rl_x/environments/custom_mujoco/robot_locomotion/mujoco/termination_functions/below_height.py
+++ rl_x/environments/custom_mujoco/robot_locomotion/mujoco/termination_functions/below_height.py
@@ -6,6 +6,6 @@ class BelowHeightTermination:
 
 
     def should_terminate(self):
-        below_height = self.env.internal_state["robot_imu_height_over_ground"] < (self.env.internal_state["env_curriculum_coeff"] * self.height_percentage_threshold * self.env.internal_state["robot_nominal_imu_height_over_ground"])
+        below_height = self.env.internal_state["robot_imu_height_over_ground"] < (self.height_percentage_threshold * self.env.internal_state["robot_nominal_imu_height_over_ground"])
 
         return below_height
